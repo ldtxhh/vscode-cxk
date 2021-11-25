741afe326d927f7754176492d4390ebabc3140ec
devld <devlduio@gmail.com>
2021-11-25 10:47:16 +0800

    修改

--- src/extension.ts
+++ src/extension.ts
@@ -7,13 +7,13 @@ export function activate(context: vscode.ExtensionContext) {
     const scheduler = new Scheduler(context);
     scheduler.start();
 
-    let imageNameArray = ['cxk_ball.gif','cxk_band.gif','cxk_blossom.gif','cxk_dyz.gif','cxk_gong.gif','cxk_hit.gif','cxk_king.gif','cxk_matches.gif','cxk_RUOK.gif','cxk_xgk.gif','cxk.gif'];
+    let imageNameArray = Array(1).fill(null).map((_, i) => `${i}.gif`);
 
-    context.subscriptions.push(vscode.commands.registerCommand('cxk.setReminderView', () => {
-            ReminderView.show(context,imageNameArray[Math.floor(Math.random()*11)]);
+    context.subscriptions.push(vscode.commands.registerCommand('xz.setReminderView', () => {
+            ReminderView.show(context,imageNameArray[Math.floor(Math.random()*imageNameArray.length)]);
     }));
     
-    context.subscriptions.push(vscode.commands.registerCommand('cxk.showReminderView', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('xz.showReminderView', () => {
         ReminderView.show(context);
     }));
 }
