303244e53b88f1f234f963324105e0ccf5785ff6
devld <devlduio@gmail.com>
2021-11-25 11:03:13 +0800

    增加图片

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
     const scheduler = new Scheduler(context);
     scheduler.start();
 
-    let imageNameArray = Array(1).fill(null).map((_, i) => `${i}.gif`);
+    let imageNameArray = Array(5).fill(null).map((_, i) => `${i}.gif`);
 
     context.subscriptions.push(vscode.commands.registerCommand('xz.setReminderView', () => {
             ReminderView.show(context,imageNameArray[Math.floor(Math.random()*imageNameArray.length)]);
